e26235fddbbedd003fc963be27f55be6d42b54ae
Bashar <omushb@gmail.com>
2023-05-02 10:04:53 +0300

    update  react fils (add remove buuton )

--- bank-app/src/App.js
+++ bank-app/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Forms from './form';
-import Home from './home';
 import Layout from './Layout';
 import './App.css';
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
         <Route path="forms" element={<Forms />} />
       </Route>
     </Routes>
--- bank-app/src/form.js
+++ bank-app/src/form.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import ReactDOM from "react-dom/client";
 const Forms = () => {
     const initState = {
         accounts : [
