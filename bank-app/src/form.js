31b38e730484afcb55b3b8b5f8df2cafebb901fb
Bashar <omushb@gmail.com>
2023-05-02 09:20:09 +0300

    update  react fils (add remove buuton )

--- bank-app/src/form.js
+++ bank-app/src/form.js
@@ -1,10 +1,6 @@
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
 const Forms = () => {
-
-   
-
-
     const initState = {
         accounts : [
                 {
@@ -52,36 +48,6 @@ const handleDelete = (id) => {
         
        
       };
-        // const Cards=(initState.accounts)=>{
-        //     return(
-               
-        //         initState.accounts.forEach((e)=> {
-        //             <p> e.customerName </p>
-        //     //   <div className="row justify-content-center me-0 mt-3" style={{marginLeft:"25rem"}}>
-        //     //      console.log(initState.accounts);
-        //     //     <div className="col-8  ">
-        //     //     <div className="card" style={{ width: "18rem" }}>
-        //     //   <div className="card-body">
-        //     //     <h5 className="card-title">your name is {index.id}</h5>
-        //     //     <p className="card-text">
-        //     //       Some quick example text to build on the card title and make up the bulk of
-        //     //       the card's content.
-        //     //     </p>
-        //     //   </div>
-        //     //   <ul className="list-group list-group-flush">
-        //     //     <li className="list-group-item">An item</li>
-        //     //     <li className="list-group-item">A second item</li>
-        //     //     <li className="list-group-item">A third item</li>
-        //     //   </ul>
-        //     //   <div className="card-body">
-        //     //     <a href="#" className="card-link">
-        //     //     </a>
-        //     //   </div>
-        //     // </div>
-        //     // </div></div>
-        // }))
-        //     }
-    
     return (
     
     <>
